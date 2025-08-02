f3ddbbbe0c6b684cae9d49eeec3e6945982029c8
iguessfive <iguessfive@outlook.com>
2025-08-02 15:54:35 +0100

    fix: correct message shown at end of level

--- ui/level_end.gd
+++ ui/level_end.gd
@@ -1,9 +1,15 @@
 extends Control
 
-var has_successfully_completed_level := false
+const LEVEL_CLEARED_TEXT = "Level Cleared!"
+const LEVEL_FAILED_TEXT = "Level Failed!"
+
+var has_successfully_completed_level: bool = false
 
 @onready var play_again_button: Button = $PlayAgainButton
 @onready var dialogue_box: RichTextLabel = $DialogueBox
+
+func _ready() -> void:
+	$ResultLabel.text = LEVEL_CLEARED_TEXT if has_successfully_completed_level else LEVEL_FAILED_TEXT
 
 func _on_level_selector_button_pressed() -> void:
 	get_tree().change_scene_to_file(FilePath.LEVEL_SELECTOR)
@@ -13,4 +19,3 @@ func _on_dialogue_box_show_dialogue() -> void:
 		dialogue_box.animate_dialogue(dialogue_box.succeeded_text)
 	else:
 		dialogue_box.animate_dialogue(dialogue_box.failed_text)
-
