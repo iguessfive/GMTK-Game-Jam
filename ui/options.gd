3a29e56b77a63e6223f42891eb4d0334eb78cc2e
iguessfive <iguessfive@outlook.com>
2025-08-02 13:22:58 +0100

    Added toggle to enable/disable sfx

--- ui/options.gd
+++ ui/options.gd
@@ -1,5 +1,11 @@
 extends Control
 
+func _ready() -> void:
+	_on_sfx_check_button_toggled(true)
+
+func _on_sfx_check_button_toggled(toggled_on: bool) -> void:
+	var sfx = AudioServer.get_bus_index("SFX")
+	AudioServer.set_bus_mute(sfx, toggled_on)
 
 func _on_level_selector_button_pressed() -> void:
-	get_tree().change_scene_to_file(FilePath.LEVEL_SELECTOR)
+	get_tree().change_scene_to_file(FilePath.LEVEL_SELECTOR) 
