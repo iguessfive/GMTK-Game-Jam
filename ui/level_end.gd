78365d1e1ba57cc9c7f9b5f824db4fa0a60fa89a
darkdollyumi <darkdollyumi@gmail.com>
2025-08-03 01:02:27 +0800

    Add files via upload
    added level end screen and buttons, although play again button does not connect yet

--- ui/level_end.gd
+++ ui/level_end.gd
@@ -2,14 +2,20 @@ extends Control
 
 var has_successfully_completed_level := false
 
-@onready var play_again_button: Button = $PlayAgainButton
+@onready var play_again = $"HBoxContainer/VBoxContainer/play again"
 @onready var dialogue_box: RichTextLabel = $DialogueBox
 
-func _on_level_selector_button_pressed() -> void:
-	get_tree().change_scene_to_file(FilePath.LEVEL_SELECTOR)
 
 func _on_dialogue_box_show_dialogue() -> void:
 	if has_successfully_completed_level:
 		dialogue_box.animate_dialogue(dialogue_box.succeeded_text)
 	else:
 		dialogue_box.animate_dialogue(dialogue_box.failed_text)
+
+
+func _on_play_again_pressed():
+	pass # Replace with function body.
+
+
+func _on_other_level_pressed() -> void:
+	get_tree().change_scene_to_file(FilePath.LEVEL_SELECTOR)
