6a34db9ae01d0ebffaa3a8279e03f08f26d5a6e2
darkdollyumi <darkdollyumi@gmail.com>
2025-08-03 14:58:16 +0800

    Add files via upload
    generic update

--- autoloads/file_path.gd
+++ autoloads/file_path.gd
@@ -1,5 +1,6 @@
 extends Node
 
-const LEVEL_SELECTOR = StringName("ui/level_selector.tscn")
+const LEVEL_SELECTOR = StringName("ui/level_selector_demo.tscn")
 
-const ANCIENT_EGYPT = StringName("res://levels/ancient_egypt_level.tscn")
+const ANCIENT_EGYPT_LEVEL_1 = StringName("res://levels/ancient_egypt/level_1.tscn")
+const ANCIENT_EGYPT_LEVEL_2 = StringName("res://levels/ancient_egypt/level_2.tscn")
