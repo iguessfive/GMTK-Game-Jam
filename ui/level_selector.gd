4f7b755844046e269b02ccff79665138d32e7ff9
darkdollyumi <darkdollyumi@gmail.com>
2025-08-03 17:23:01 +0800

    level selector draft

--- ui/level_selector.gd
+++ ui/level_selector.gd
@@ -1,8 +1,17 @@
 extends Control
 
+
+
 func _on_ancient_egypt_pressed() -> void:
 	get_tree().change_scene_to_file(FilePath.ANCIENT_EGYPT_LEVEL_1)
 
 
 func _on_ancient_egypt_2_pressed() -> void:
 	get_tree().change_scene_to_file(FilePath.ANCIENT_EGYPT_LEVEL_2)
+
+
+func _on_egyptframe_mouse_entered():
+	$GridContainer/egyptcontainer.visible = true
+	
+func _on_egyptframe_mouse_exited():
+	$GridContainer/egyptcontainer.visible = false
