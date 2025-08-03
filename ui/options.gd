5572467a5b988e841b0ac7d81926dde387e63598
iguessfive <iguessfive@outlook.com>
2025-08-03 11:33:53 +0100

    bug: sfx setting not persistent across game

--- ui/options.gd
+++ ui/options.gd
@@ -3,11 +3,18 @@ extends Control
 const SPEAKER_ON = preload("res://assets/icons/speaker_on.png")
 const SPEAKER_OFF = preload("res://assets/icons/speaker_off.png")
 
+static var is_muted: bool
+
+func _ready() -> void:
+	$SFXCheckButton.icon = SPEAKER_OFF if is_muted else SPEAKER_ON
+	$SFXCheckButton.button_pressed = is_muted
+	
 func _on_sfx_check_button_toggled(toggled_on: bool) -> void:
+	is_muted = toggled_on
 	var sfx = AudioServer.get_bus_index("SFX")
 	AudioServer.set_bus_mute(sfx, toggled_on)
 	
-	$SFXCheckButton.icon = SPEAKER_OFF if toggled_on else SPEAKER_ON
+	$SFXCheckButton.icon = SPEAKER_OFF if is_muted else SPEAKER_ON
 
 func _on_level_selector_button_pressed() -> void:
 	get_tree().change_scene_to_file(FilePath.LEVEL_SELECTOR) 
