d267d3eaaf1c78c2e1c3d7ed3bcef7711a05bf1c
iguessfive <iguessfive@outlook.com>
2025-08-03 10:35:19 +0100

    added mute icon change

--- ui/options.gd
+++ ui/options.gd
@@ -1,8 +1,13 @@
 extends Control
 
+const SPEAKER_ON = preload("res://assets/icons/speaker_on.png")
+const SPEAKER_OFF = preload("res://assets/icons/speaker_off.png")
+
 func _on_sfx_check_button_toggled(toggled_on: bool) -> void:
 	var sfx = AudioServer.get_bus_index("SFX")
 	AudioServer.set_bus_mute(sfx, toggled_on)
+	
+	$SFXCheckButton.icon = SPEAKER_OFF if toggled_on else SPEAKER_ON
 
 func _on_level_selector_button_pressed() -> void:
 	get_tree().change_scene_to_file(FilePath.LEVEL_SELECTOR) 
